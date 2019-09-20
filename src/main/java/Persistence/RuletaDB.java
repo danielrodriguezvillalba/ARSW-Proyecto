c72e8551bed41858160978bddc9398506b5a303a
Miguel Rojas <miromar94@hotmail.com>
2019-09-20 17:23:02 -0500

    Conexion al DB y Insert de usuarios

--- src/main/java/Persistence/RuletaDB.java
+++ src/main/java/Persistence/RuletaDB.java
@@ -5,8 +5,10 @@
  */
 package Persistence;
 
+import application.Usuario;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -16,31 +18,113 @@ import java.sql.Statement;
  */
 public class RuletaDB {
     //Direccion del DB de prueba
-    String urlDatabase =  "jdbc:postgresql://localhost:5432/miRuleta";
-    Connection conn = null;
+    static String urlDatabase = "jdbc:postgresql://ec2-23-21-115-109.compute-1.amazonaws.com:5432/dcjlcvlpuum728";
+    static String usuarioDb = "unkwzcvdxtrfys";
+    static String passwordDb= "03eaed5577b5eadb70f2bbe4de0d68e23a967ab611eff45138dc2b690b0fe052";
+    static Connection conn = null;
+    
+    /**
+     * Main provicional para realizar pruebas a la base de datos
+    public static void main(String args[]) {
+        Usuario user = new Usuario(768903,"Didio", "Osorio", "dosorio@sywork.net","contra");
+        realizaConexion();
+        insertarUsuario(user);
+        //createTable();
+        //insertarRegistro();
+        //selectRegistros();
+    }**/
 
 
-    public void realizaConexion(){
+    public static void realizaConexion(){
         try {
             Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection(urlDatabase,  "usuario", "password");
+            conn = DriverManager.getConnection(urlDatabase,  usuarioDb, passwordDb);
         } catch (ClassNotFoundException e) {
-            System.out.println("Ocurrio un error : "+e.getMessage());
+            System.out.println("Ocurrio un error not found: "+e.getMessage());
         } catch (SQLException e) {
             System.out.println("Ocurrio un error : "+e.getMessage());
         }
-        System.out.println("La conexión se realizo sin problemas! =) ");
+        System.out.println("La conexión se realizo sin problemas!");
     }
     
-    public void insertarRegistro(){
+    public static void createTable(){
         //Recupera conexion y crea Statement para el db
-        Connection c = this.conn;
+        Connection c = conn;
         Statement stmt = null;
         
         //Valida si existe una conexion abierta al db y si no trata de abrir una
         if(c == null){
             try {
-                c = DriverManager.getConnection(this.urlDatabase,"manisha", "123");
+                c = DriverManager.getConnection(urlDatabase,usuarioDb, passwordDb);
+                c.setAutoCommit(false);
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error : "+e.getMessage());
+            }
+            System.out.println("La conexión se realizo sin problemas! =) ");
+        }
+        try{
+            Class.forName("org.postgresql.Driver");
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE COMPANY " +
+               "(ID INT PRIMARY KEY     NOT NULL," +
+               " NAME           TEXT    NOT NULL, " +
+               " AGE            INT     NOT NULL, " +
+               " ADDRESS        CHAR(50), " +
+               " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
+  
+    }
+    
+    public static void insertarUsuario(Usuario user){
+        //Recupera conexion y crea Statement para el db
+        Usuario usr = user;
+        Connection c = conn;
+        Statement stmt = null;
+        
+        //Valida si existe una conexion abierta al db y si no trata de abrir una
+        if(c == null){
+            try {
+                c = DriverManager.getConnection(urlDatabase,usuarioDb, passwordDb);
+                
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error : "+e.getMessage());
+            }
+            System.out.println("La conexión se realizo sin problemas! =) ");
+        }
+        try{
+            Class.forName("org.postgresql.Driver");
+            c.setAutoCommit(false);
+            stmt = c.createStatement();
+            //SQL de ejemplo mientras se define lo que se va a ingresar a la base de datos
+            String sql = "INSERT INTO usuario (nombre,apellido,correo,contraseña,saldo,numerocedula) "
+               +"VALUES ('"+usr.getNombre()+"','"+usr.getApellido()+"','"+usr.getCorreo()+"','"+usr.getContra()+"',"+usr.getSaldo()+","+usr.getId()+");";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.commit();
+            c.close();
+        }catch(Exception e){
+            System.out.println("Ocurrio un error ingresando el usuario : "+e.getMessage());
+        }
+        System.out.println("Se ingreso correctamente el usuario");
+    }
+    
+    
+    public static void insertarRegistro(){
+        //Recupera conexion y crea Statement para el db
+        Connection c = conn;
+        Statement stmt = null;
+        
+        //Valida si existe una conexion abierta al db y si no trata de abrir una
+        if(c == null){
+            try {
+                c = DriverManager.getConnection(urlDatabase,usuarioDb, passwordDb);
                 c.setAutoCommit(false);
             } catch (Exception e) {
                 System.out.println("Ocurrio un error : "+e.getMessage());
@@ -49,20 +133,61 @@ public class RuletaDB {
         }
         
         try {
-         Class.forName("org.postgresql.Driver");
-         stmt = c.createStatement();
-         //SQL de ejemplo mientras se define lo que se va a ingresar a la base de datos
-         String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
-            + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
-         stmt.executeUpdate(sql);
-         stmt.close();
-         c.commit();
-         c.close();
-      } catch (Exception e) {
-         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
-         System.exit(0);
-      }
-      System.out.println("Records created successfully");
+            Class.forName("org.postgresql.Driver");
+            stmt = c.createStatement();
+            //SQL de ejemplo mientras se define lo que se va a ingresar a la base de datos
+            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+               + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch (Exception e) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
          
+    }
+    
+    public static void selectRegistros(){
+        //Recupera conexion y crea Statement para el db
+        Connection c = conn;
+        Statement stmt = null;
+        
+        //Valida si existe una conexion abierta al db y si no trata de abrir una
+        if(c == null){
+            try {
+                c = DriverManager.getConnection(urlDatabase,usuarioDb, passwordDb);
+                c.setAutoCommit(false);
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error : "+e.getMessage());
+            }
+            System.out.println("La conexión se realizo sin problemas! =) ");
+        }
+        try {
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+            while ( rs.next() ) {
+                int id = rs.getInt("id");
+                String  name = rs.getString("name");
+                int age  = rs.getInt("age");
+                String  address = rs.getString("address");
+                float salary = rs.getFloat("salary");
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "AGE = " + age );
+                System.out.println( "ADDRESS = " + address );
+                System.out.println( "SALARY = " + salary );
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
     }
 }
