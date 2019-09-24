4003d130429df9b8c264429b5a775177a4d89201
Tylones <etiennereitz@gmail.com>
2019-09-24 03:53:55 +0200

    Updated tests for ganancia

--- src/test/java/application/ApuestaTest.java
+++ src/test/java/application/ApuestaTest.java
@@ -22,13 +22,30 @@ public class ApuestaTest{
         apuesta.apostar("1", 10.0);
         assertEquals("Betting with a unique number not good", Double.valueOf(360.0), apuesta.ganancia("1"));
 
-        // Betting with a unique number and 2to1 not good
+        // Betting with a unique number and 2to1 
         apuesta.apostar("2to1-1", 10.0);
         assertEquals("Betting with a unique number and 2to1 not good", Double.valueOf(360.0+20), apuesta.ganancia("1"));
 
-        // Betting with a unique number and 2to1 and red not good
+        // Betting with a unique number and 2to1 and red
         apuesta.apostar("red", 10.0);
         assertEquals("Betting with a unique number and 2to1 and red not good", Double.valueOf(360.0+20), apuesta.ganancia("1"));
+
+        // Betting with a unique number and 2to1 and red and black (black loosing)
+        apuesta.apostar("red", 10.0);
+        assertEquals("Betting with a unique number and 2to1 and red and black (black loosing) not good", Double.valueOf(360.0+20), apuesta.ganancia("1"));
+
+        // Betting on two numbers
+        apuesta.apostar("2", 10.0);
+        assertEquals("Betting with two numbers not good", Double.valueOf(360.0+20), apuesta.ganancia("1"));
+
+        // Nothing wins
+        assertEquals("Nothing wins not good", Double.valueOf(360.0+20), apuesta.ganancia("00"));
+
+        // Betting on 1to18
+        apuesta.apostar("1to18", 10.0);
+        assertEquals("Betting on 1to18 not good", Double.valueOf(360.0+40.0), apuesta.ganancia("1"));
+
+        // Betting on 19to36
     }
 
 }
