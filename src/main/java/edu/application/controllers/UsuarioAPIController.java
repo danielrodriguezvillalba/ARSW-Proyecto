38a9f0a2bf091d5dfcec8d6650305013df04eea3
Tylones <etiennereitz@gmail.com>
2019-10-08 18:49:40 -0500

    Password are not hashed in the database

--- src/main/java/edu/application/controllers/UsuarioAPIController.java
+++ src/main/java/edu/application/controllers/UsuarioAPIController.java
@@ -6,6 +6,9 @@ import edu.application.model.Usuario;
 import edu.application.Exceptions.RoulettePersistenceException;
 import edu.application.services.Services;
 import edu.application.services.impl.UsuarioServices;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -45,23 +48,38 @@ public class UsuarioAPIController{
             JSONObject obj = new JSONObject(body);
             String username = obj.getString("email");
             String password = obj.getString("password");
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] bytes = digest.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < bytes.length; i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            String psswdHash = sb.toString();
             Usuario u = (Usuario) services.getElement(username);
-            if(u.getContra().equals(password))
+            if(u.getContra().equals(psswdHash))
                 return new ResponseEntity<>(services.getElement(username), HttpStatus.OK);
             else
                 return new ResponseEntity<>("Wrong password", HttpStatus.UNAUTHORIZED);
 
-        } catch (RoulettePersistenceException ex) {
+        } catch (RoulettePersistenceException | NoSuchAlgorithmException ex) {
             Logger.getLogger(UsuarioAPIController.class.getName()).log(Level.SEVERE, null, ex);
             return new ResponseEntity<>("Error bla bla bla", HttpStatus.NOT_FOUND);
         }
     }
     
     @RequestMapping(path = "/addUser", method = RequestMethod.POST)
-    public ResponseEntity<?> InsertUsers(@RequestBody String body) {
+    public ResponseEntity<?> InsertUsers(@RequestBody String body) throws NoSuchAlgorithmException {
         //obtener datos que se enviarán a través del API
         JSONObject obj = new JSONObject(body);
-        Usuario us = new Usuario(obj.getInt("TaxID"), obj.getString("name"), obj.getString("lastname"), obj.getString("email"), obj.getString("password1"));
+        String password = obj.getString("password1");
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = digest.digest(password.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < bytes.length; i++){
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        String psswdHash = sb.toString();
+        Usuario us = new Usuario(obj.getInt("TaxID"), obj.getString("name"), obj.getString("lastname"), obj.getString("email"), psswdHash);
         System.out.println("Va a ingreas");
         try {
             services.insertarUsuario(us);
