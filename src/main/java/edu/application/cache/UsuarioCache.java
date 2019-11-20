3be4af348bb102ba98a0a6d4f505675385583a36
Tylones <etiennereitz@gmail.com>
2019-11-20 08:06:13 -0500

    Synchronized cache

--- src/main/java/edu/application/cache/UsuarioCache.java
+++ src/main/java/edu/application/cache/UsuarioCache.java
@@ -15,19 +15,27 @@ public class UsuarioCache {
     private static Map<String, Usuario> usuarioMap = new HashMap<String, Usuario>();
 
     public Usuario getCachedUsuario(String email){
-        return usuarioMap.get(email);
+        synchronized (usuarioMap){
+            return usuarioMap.get(email);
+        }
     }
 
     public boolean containsUsuario(String email){
-        return usuarioMap.containsKey(email);
+        synchronized (usuarioMap) {
+            return usuarioMap.containsKey(email);
+        }
     }
 
     public void putUsuarioCache(Usuario us){
-        usuarioMap.put(us.getCorreo(),us);
+        synchronized (usuarioMap) {
+            usuarioMap.put(us.getCorreo(), us);
+        }
     }
 
     public void updateUsuarioSaldo(String email, float value){
-        float oldValue = usuarioMap.get(email).getSaldo();
-        usuarioMap.get(email).setSaldo(oldValue + value);
+        synchronized (usuarioMap){
+            float oldValue = usuarioMap.get(email).getSaldo();
+            usuarioMap.get(email).setSaldo(oldValue + value);
+        }
     }
 }
